52a1e34ac82b48cb43a9b70a77f522f99e2b21ca
Ricky Luna <ricky@delosdata.com>
2026-07-02 17:50:22 -0600

    Fixing DB

--- prisma/migrations/20260702000001_rename_app_release/migration.sql
+++ prisma/migrations/20260702000001_rename_app_release/migration.sql
@@ -1,3 +1,8 @@
-ALTER TABLE "app_release" RENAME TO "AppRelease";
-ALTER INDEX "app_release_uploaded_at_idx" RENAME TO "AppRelease_uploaded_at_idx";
-ALTER TABLE "AppRelease" RENAME CONSTRAINT "app_release_pkey" TO "AppRelease_pkey";
+DO $$
+BEGIN
+  IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'app_release') THEN
+    ALTER TABLE "app_release" RENAME TO "AppRelease";
+    ALTER INDEX "app_release_uploaded_at_idx" RENAME TO "AppRelease_uploaded_at_idx";
+    ALTER TABLE "AppRelease" RENAME CONSTRAINT "app_release_pkey" TO "AppRelease_pkey";
+  END IF;
+END $$;
